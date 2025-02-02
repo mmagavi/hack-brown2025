1ceb513aac924cf30786c0495863be069b24eba0
GiannaDyer <gianna_dyer@brown.edu>
2025-02-02 11:04:36 -0500

    before updated prompt

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ let post_title = '';
 let post_content = [];
 let manual_input = '';
 let has_manual_input = false;
-
+let api_key = '';
 let response = '';
 
 // Get the current tab URL, and scrape the site for the post title and content
@@ -17,6 +17,14 @@ async function start() {
     const textFieldDiv = document.getElementById('text-field');
     const my_string = "\"" + post_title + '-' + post_content + "... \""
     textFieldDiv.textContent = truncateString(my_string, 195);
+}
+
+function getKey() {
+    fetch("api-key.txt")
+        .then(response => response.text())
+    .then(data => {
+        api_key = data;
+    });
 }
 
 // Scraping the Reddit page for the post title and content
@@ -93,9 +101,13 @@ async function generateResponse() {
                     // System role defines behavior + context (prompt)
                     { role: "system", content: `You are a user reading an online forum site about a certain disease. 
                     You want to be sure that the posts you are reading are not written in a misleading way or by relying heavily 
-                    on anecdotes to make conclusions. Read this post, and find the central claim and the pieces of evidence used to support it. 
-                    Classify each of those pieces of evidence as one of the following: anecdotal, unlikely to be supported by evidence, 
-                    misleading, untrue, likely to be supported by evidence, not misleading, true.` },
+                    on anecdotes to make conclusions about medical diagnoses. Read this post, and decide if it is making a claim about
+                    medical diagnoses, procedures, or general medical information. If so, find the central claim and the most important
+                    pieces of evidence used to support it. Classify each of those pieces of evidence as one of the following: anecdotal,
+                    unlikely to be supported by evidence, misleading, untrue, likely to be supported by evidence, not misleading, true.
+                    If the post is not making a claim about medical diagnoses, procedures, or general medical information, find the
+                    central claim and the pieces of evidence used to support it. Use bold text and bullet points to organize your
+                    response.` },
                     {
                     // User role defines first message in the chat (post to respond to)
                         role: "user",
